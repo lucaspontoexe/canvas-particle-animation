5c1cff698782f825c9a75d8bc38c9d3f48cef476
lucaspontoexe <ltx16@hotmail.com>
2023-02-12 17:42:56 -0300

    calculate particles instead of hardcoding 1500

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,19 @@
 import { canvas, context as c } from "./canvas.js";
 import Particle from "./particle.js";
-let slider = document.querySelector('input[type=range]');
+let slider = document.querySelector("input[type=range]");
 
 let particle_y_speed = 4;
 let particles = [];
 
-function initStars() {
+const calculateParticleCount = () => (innerWidth * innerHeight) / 1200;
+
+function initStars(count =  1500) {
   canvas.width = innerWidth;
   canvas.height = innerHeight;
 
   particles = [];
 
-  for (let i = 0; i < 1500; i++) {
+  for (let i = 0; i < count; i++) {
     particles.push(new Particle());
   }
   // otimizar etc
@@ -29,7 +31,8 @@ function animate() {
   requestAnimationFrame(animate);
 }
 
-initStars();
+initStars(calculateParticleCount());
 animate();
-window.addEventListener("resize", initStars);
-slider.addEventListener("input", e => particle_y_speed = +e.target.value);
+
+window.addEventListener("resize", () => initStars(calculateParticleCount()));
+slider.addEventListener("input", (e) => (particle_y_speed = +e.target.value));
