2a88ce04c7ad15ca1834a3afd2bd04a8e869065d
lucaspontoexe <ltx16@hotmail.com>
2023-02-12 19:23:12 -0300

    add fps measurer

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,15 @@
 import { canvas, context as c } from "./canvas.js";
 import Particle from "./particle.js";
 let slider = document.querySelector("input[type=range]");
+let fps_display = document.querySelector("span.fps");
 
 let particle_y_speed = 4;
 let particles = [];
+let last_frame_timestamp = performance.now();
 
 const calculateParticleCount = () => (innerWidth * innerHeight) / 1200;
 
-function initStars(count =  1500) {
+function initStars(count = 1500) {
   canvas.width = innerWidth;
   canvas.height = innerHeight;
 
@@ -21,6 +23,8 @@ function initStars(count =  1500) {
 }
 
 function animate() {
+  const this_frame_timestamp = performance.now();
+
   c.fillStyle = "rgba(0,0,0,0.25)";
   c.fillRect(0, 0, canvas.width, canvas.height);
 
@@ -28,6 +32,8 @@ function animate() {
     particle.update(particle_y_speed);
   });
 
+  fps_display.textContent = Math.round(1000 / (this_frame_timestamp - last_frame_timestamp));
+  last_frame_timestamp = this_frame_timestamp;
   requestAnimationFrame(animate);
 }
 
