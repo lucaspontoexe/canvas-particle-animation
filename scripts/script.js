3a988c43f31e1d45d74b5bedb82275469915d3be
lucaspontoexe <611847+lucaspontoexe@users.noreply.github.com>
2023-02-21 21:05:13 -0300

    use audio to drive waves

--- scripts/script.js
+++ scripts/script.js
@@ -44,3 +44,40 @@ animate();
 window.addEventListener("resize", () => initStars(calculateParticleCount()));
 Yslider.addEventListener("input", (e) => (particle_y_speed = +e.target.value));
 Xslider.addEventListener("input", (e) => (particle_x_speed = +e.target.value));
+
+// audio test
+const audioElement = document.getElementsByTagName("audio")[0];
+
+audioElement.addEventListener("play", visualize);
+
+function visualize() {
+  const audioCtx = new AudioContext();
+  const source = audioCtx.createMediaElementSource(audioElement);
+
+  const analyser = audioCtx.createAnalyser();
+  source.connect(analyser);
+  analyser.connect(audioCtx.destination);
+
+  analyser.fftSize = 32;
+  const bufferLength = analyser.frequencyBinCount;
+  const dataArray = new Uint8Array(bufferLength);
+
+  // https://losseff.xyz/katas/013-squeare-sum/javascript/
+  function squareSum(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+      sum += numbers[i] ** 2;
+    }
+    return Math.sqrt(sum);
+  }
+
+  function draw() {
+    analyser.getByteFrequencyData(dataArray);
+    particle_x_speed = squareSum(dataArray.slice(4)) / 128;
+    particle_y_speed = -4 + squareSum(dataArray.slice(0, 3)) / 128;
+    Xslider.value = particle_x_speed;
+    Yslider.value = particle_y_speed;
+    requestAnimationFrame(draw);
+  }
+  draw();
+}
